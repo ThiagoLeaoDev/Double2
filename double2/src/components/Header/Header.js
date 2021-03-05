54117db833f733944e906cb9c4f1411ddf7c8a99
Qu4ntumT <thiago.lesk@outlook.com>
2021-03-05 00:09:28 -0300

    Initialization styled-components

---
+++ double2/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Tabbar from "./HeaderStyle"
+
+// import { Container } from './styles';
+
+function Header() {
+  return(
+    <Tabbar>
+    </Tabbar>
+  );
+}
+
+export default Header;
---
+++ double2/src/components/Header/HeaderStyle.js
@@ -0,0 +1,7 @@
+import styled from "styled-components"
+
+const Tabbar = styled.header`
+  background-color: "#0D0D0F";
+  width: "100%";
+  height: 200px;
+`;
